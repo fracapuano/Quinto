b3b3a2888768dc4dbdb4b53c1cf20ba7d2c16185
mttmtt31 <matteomatteotti1@gmail.com>
2022-12-23 12:47:26 +0100

    add: documentation

---
+++ quartoenv/env.py
@@ -0,0 +1,249 @@
+import gym
+from gym.spaces import MultiDiscrete
+import logging
+import numpy as np
+
+from typing import Union, Iterable
+
+from .game import QuartoGame, QuartoPiece, QUARTO_DICT
+
+logger = logging.getLogger(__name__)
+
+class QuartoEnv(gym.Env):
+    EMPTY = 0
+    metadata = {'render.modes':['terminal']}
+
+    action_space, observation_space = None, None
+
+    @property
+    def _observation(self):
+        """ State of the game after the move.
+        """
+        return (self.game.board, self.piece)
+
+    @property
+    def done(self):
+        return self.broken or self.game.game_over or self.game.draw
+
+    def reset(self):
+        """This function takes care of resetting the environment to initial state, 
+        i.e. empty board with no pieces on."""
+        self.game = QuartoGame()
+        self.turns = 0
+        self.piece = None
+        self.broken = False
+        return self._observation
+
+    def step(self, action):
+        """This function steps the environment considering the given action"""
+        # sparse rewards - no reward but in terminal states
+        # TODO: Might be interesting to reward negatively the agent at each turn to encourage
+        #       winning early. Idea
+        reward = 0
+        # increment number of turns
+        self.turns += 1
+        info = {'turn': self.turns,
+                'invalid': False,
+                'win': False,
+                'draw': False}
+        
+        if self.done:
+            logger.warn("Actually already done")
+            return self._observation, reward, self.done, info
+
+        position, next = action
+        logger.debug(f"Received: position: {position}, next: {next}")
+
+        # self.piece stores the piece that has to be positioned on the board. 
+        # self.piece is None at first turn, i.e. at the beginning of the game.
+        
+        if self.piece is not None:  # current move is not move 0
+            # play the current piece on the board
+            valid = self.game.play(piece=self.piece, position=position, next_piece=next)
+
+            if not valid:
+                # Invalid move
+                reward = -200
+                self.broken = True
+                info['invalid'] = True
+            
+            # check if played move makes the agent win
+            elif self.game.game_over:
+                # We just won!
+                reward = 100
+                info['win'] = True
+            
+            # check draw
+            elif self.game.draw:
+                reward = 20
+                info['draw'] = True
+            else:
+                # a valid move was played
+                reward = 0
+
+        # Process the next piece
+        self.piece = next
+
+        return self._observation, reward, self.done, info
+
+
+    def render(self, mode, **kwargs):
+        for row in self.game.board:
+            s = ""
+            for piece in row:
+                if piece is None:
+                    s += ". "
+                else:
+                    s += str(piece) + " "
+            print(s)
+        print(f"Next: {self.piece}, Free: {''.join(str(p) for p in self.game.available_pieces)}")
+        print()
+
+    def __del__(self):
+        self.close()
+
+class MoveEncoderV0(gym.ActionWrapper):
+    """First version of the Move Encoding wrapping
+
+    Action is [pos, next]
+    """
+
+    def __init__(self, env:gym.Env) -> None:
+        super(MoveEncoderV0, self).__init__(env)
+        # action is [pos, next]
+        # both are not null, they are just ignored when irrelevant
+        self.action_space = gym.spaces.MultiDiscrete([16, 16])
+
+    def action(self, action):
+        """Decode an action of the type (position : int, piece : int) into an action of the type ((x, y), QuartoPiece).
+
+        Args:
+            action (Iterable): Iterable of two entries.
+                               First entry is an integer referring to the position on the board where the player wishes to place the piece chosen by the previous player.
+                               Second entry is an integer referring to the piece that the next player should place.
+        Returns:
+            _type_: decoded action, as per `decode` function.
+        """
+        return self.decode(action)
+
+    @property
+    def legal_actions(self):
+        """Encode the valid actions.
+
+        Yields:
+            Iterable: encoded valid actions.
+        """
+        for action in self.game.get_valid_actions():
+            yield self.encode(action)
+
+    def action_masks(self):
+        # get unique positions
+        valid_positions = list(set([pos for pos, _ in self.legal_actions()]))
+        # initialise masking vector -> everything is False
+        mask_pos = [False for i in range(16)]
+        # convert valid position to True
+        for valid_pos in valid_positions:
+            mask_pos[valid_pos] = True
+
+        # repeat for piece
+        # get unique pieces
+        valid_pieces = list(set([piece for _, piece in self.legal_actions()]))
+        # initialise masking vector -> everything is False
+        mask_piece = [False for i in range(16)]
+        # convert valid pieces to True
+        for valid_piece in valid_pieces:
+            mask_piece[valid_piece] = True
+        
+        return [mask_pos, mask_piece]
+        
+    action_mask_fn = action_masks
+
+    def decode(self, action : Iterable):
+        """Decode an action. Here, action is an iterable of the type (position, piece), where both position and piece are integers.
+        Converts the integer position into (x, y) coordinates and the integer piece into the corresponding Quarto piece.
+
+        Args:
+            action (Iterable): Iterable of two entries.
+                          First entry is an integer referring to the position on the board where the player wishes to place the piece chosen by the previous player.
+                          Second entry is an integer referring to the piece that the next player should place.
+
+        Returns:
+            tuple: Iterable of 2 entries of the type ((x, y), QuartoPiece)
+        """
+        # unpack action
+        position, piece = action
+        # convert integer position into (x, y) on the grid
+        position = (position // 4, position % 4)
+        # piece is None during the last move, where there is no pieces left to choose.
+        if piece is not None:
+            # convert the integer into Quarto piece
+            piece = QUARTO_DICT[piece]
+        return position, piece
+
+    def encode(self, action : Iterable):
+        """Encode an action. Here, action is an iterable of the type ((x, y), QuartoPiece).
+        Converts the (x, y) coordinates into the integer position and the QuartoPiece into the corresponding integer representation.
+
+        Args:
+            action (Iterable): Iterable of two entries.
+                             First entry are the (x, y) coordinates of the position on the board where the player wishes to place the piece chosen by the previous player.
+                             Second entry is the QuartoPiece that the next player should place.
+
+        Returns:
+            tuple: Iterable of 2 entries of the type (integer position, integer piece).
+        """
+        # unpack the action.
+        position, piece = action
+        # convert the integer position into (x, y) coordinates.
+        position = position[0] * 4 + position[1]
+        # piece is None during the last move, where there is no pieces left to choose.
+        if piece is not None:
+            # convert the Quarto piece into an integer.
+            piece = piece.index
+        return position, piece
+
+
+class QuartoEnvV0(QuartoEnv):
+    """ The encoding that were used by the v0 of the env
+    That's a subclass and not a wrapper.
+    """
+
+    def __init__(self):
+        super(QuartoEnvV0, self).__init__()
+
+        # next piece + board (flatten) 
+        # TODO: maybe [16]??
+        self.observation_space = gym.spaces.MultiDiscrete([17] * (1+4*4))
+        
+        # action is [pos, next]
+        # both are not null, they are just ignored when irrelevant
+        self.action_space = gym.spaces.MultiDiscrete([16, 16])
+
+    def step(self, action):
+        position, next = action
+        if next is not None:
+            next = QuartoPiece(next)
+        position = (position % 4, position // 4)
+        return super(QuartoEnvV0, self).step((position, next))
+
+    @property
+    def observation(self):
+        board = []
+        for row in self.game.board:
+            for piece in row:
+                if piece is None:
+                    board.append(self.EMPTY)
+                else:
+                    board.append(QuartoEnv.pieceNum(piece) + 1)
+        if self.piece is None:
+            piece = [self.EMPTY]
+        else :
+            piece = [QuartoEnv.pieceNum(self.piece) + 1]
+        return np.concatenate((piece, board)).astype(np.int8)
+
+    @property
+    def legal_actions(self):
+        for position, piece in super(QuartoEnvV0, self).legal_actions:
+            x, y = position
+            yield x+y*4, QuartoEnv.pieceNum(piece)
+
--- quartoenv/game.py
+++ quartoenv/game.py
@@ -2,6 +2,7 @@ import logging
 from typing import Union, Iterable
 import collections.abc
 import numpy as np
+from itertools import product
 
 logger = logging.getLogger(__name__)
 
@@ -9,6 +10,7 @@ class QuartoPiece:
     def __init__(self, number:int):
         """Every piece is described by a set of four binary attributes. 
         Used to create the properties describing each single piece"""
+        # to each quarto piece, an integer is associated
         self.index = number
         # each piece is either tall or short...
         self.tall = bool(number & 1)
@@ -48,10 +50,13 @@ class QuartoPiece:
             (True, True, True, True): "h"
         }[self.tall, self.hollow, self.dark, self.round]
 
+# to each Quarto piece, we can access its index.
+# However, given an index, we cannot access the corresponding Quarto piece.
+# Hence, we create a dictionary which maps each integer into a Quarto piece.
+# In this way, we can always turn one into the other.
 QUARTO_DICT = {
     idx: QuartoPiece(idx) for idx in range(16)
 }
-
 
 class QuartoGame:
     def __init__(self, init_board:Union[bool, Iterable[QuartoPiece]]=True):
@@ -63,16 +68,19 @@ class QuartoGame:
         """
         "TODO: Add check on initboard iterable containing only QuartoPiece objects"
 
+        # all possible pieces, from QuartoPiece(0) to QuartoPiece(15)
         self.all_pieces = [QuartoPiece(i) for i in range(16)]
 
         if isinstance(init_board, collections.abc.Iterable):  # start from a given configuration
             # accept board as input
-            self.board = init_board 
-            self.available = [piece for piece in self.all_pieces if piece not in self.board]
+            self.board = init_board
+            # if board is given, onnly some pieces will be available 
+            self.available_pieces = [piece for piece in self.all_pieces if piece not in self.board]
         else:  
-            # initialize board from scratch
+            # initialize board from scratch. By default, an empty spot will be associated will value 0.
             self.board = np.zeros((4,4))
-            self.available = self.all_pieces
+            # all pieces are available
+            self.available_pieces = self.all_pieces
 
     def play(self, piece: QuartoPiece, position: int, next_piece: QuartoPiece) -> bool:
         """This function plays a move given piece and position as per game rules. Moreover, it also removes 
@@ -91,7 +99,7 @@ class QuartoGame:
             raise ValueError(f"Piece {piece} is not an instance of QuartoPiece!")
         
         # check if piece is available
-        if piece not in self.available:
+        if piece not in self.available_pieces:
             logger.warn(f"Not placing a free piece, {piece}, {''.join(str(p) for p in self.free)}")
             # still playing
             return False
@@ -99,32 +107,39 @@ class QuartoGame:
         # turning position into single coordinates
         x, y = position
 
-        # check if position free
+        # check if position is available
         logger.debug(f"Willing to play at ({x},{y})")
         if self.board[x, y] != 0:
             # cell is not empty
             logger.warn(f"Not on a free spot {self.board[x, y]} at {x}, {y}")
             return False
         
-        # play piece in position
+        # play piece in position. On the board, we are not placing the QuartoPiece, but the corresponding index.
         self.board[x, y] = piece.index
         # remove chosen piece from available ones
-        self.available.remove(piece)
+        self.available_pieces.remove(piece)
         
-        # check if game is in terminal state
-        if not (self.game_over or self.draw) and next_piece not in self.available:
+        # check if the game is still going on AND next_piece is not available.
+        # This means the action you are playing is not valid.
+        if not (self.game_over or self.draw) and next_piece not in self.available_pieces:
             # either pieces is empty (no more pieces from which to chose) or game is over
             logger.warn(f"Next piece invalid, {next_piece}, {''.join(str(p) for p in self.free)}")
             return False
         
-        return True  # terminal state
+        # you played a valid move. The game might also be finished.
+        return True  
 
     @property
     def game_over(self):
-        """This checks if the game is in terminal state because a terminal configuration
-        (one exibing one common traits on any row-column or main diagonal) has been reached.
+        """Check if someone won.
+
+        Returns:
+            bool: True if someone won (i.e., 4 pieces sharing a property are aligned), False otherwise.
         """
         # checking over possible rows
+        # There are 4 rows and 4 columns
+        # Iteratively check if 4 pieces along a line share a common property.
+        # Start by checking horizontal and vertical lines.
         for i in range(4):
             if QuartoGame.common(*self.board[i, :]):
                 logger.info(f" {i}-th row: common trait")
@@ -133,7 +148,7 @@ class QuartoGame:
                 logger.info(f" {i}-th columns: common trait")
                 return True
         
-        # checking over the two diagonals
+        # Check the 2 diagonals.
         main_diagonal = [self.board[idx, idx] for idx in range(4)]
         inverse_main_diagonal = [self.board[idx, 3-idx] for idx in range(4)]
 
@@ -145,33 +160,67 @@ class QuartoGame:
             logger.info("inverse main diagonal: common trait")
             return True
         
-        # board is not exhibiting any common trait
+        # board is not exhibiting any common trait, nobody won.
         return False
 
     @property
     def draw(self):
-        """ Game is finished but no one won
+        """ Check if the game is a tie, i.e., all pieces are places but nobody won.
+
+        Returns:
+            bool: True if the game is a tie, False otherwise.
         """
-        # board is full of pieces not exhibiting any common trait
+        # nobody has won yet AND the board is full.
         return (not self.game_over) and (0 not in self.board)
 
     @property
     def free_spots(self):
-        """Return free spots on the board"""
+        """Return free spots on the board.
+        
+        Returns:
+            np.array: array containing tuples (x, y) with the coordinates of the available positions on the board
+        """
         return np.argwhere(self.board == 0)  # 0 identifies an empty spot
     
     def common(a, b, c, d):
-        """ The four piece have a common property
+        """ Given four pieces along a line, check if they share a property.
+
+        Returns:
+            bool: True if the 4 pieces share a property, False otherwise.
         """
+        # NECESSARY (but not sufficient) CONDITION: there must be 4 pieces along the considered line.
         if 0 in (a, b, c, d):  # one of the considered position is empty
             return False
 
-        """TODO: Turn getattr(a, attr) == ... == getattr(d, attr) into something one could be proud of"""
+        # turn the pieces (which are integers atm) into the corresponding Quarto pieces
         piece_a, piece_b, piece_c, piece_d = [QUARTO_DICT[idx] for idx in [a,b,c,d]]
+        # check if the 4 Quarto pieces share any of the attributes.
         for attribute in ["tall", "hollow", "dark", "round"]: 
             common_trait = getattr(piece_a, attribute) == getattr(piece_b, attribute) == getattr(piece_c, attribute) == getattr(piece_d, attribute)
             if common_trait: 
-                return True
-        
+                return True        
         # no common trait has been found among the given pieces
         return False
+
+    @property
+    def get_valid_actions(self):
+        """In any situation of the game, returns the available actions as ((x, y), QuartoPiece)
+        
+        Returns:
+            np.array: array of tuples of the type (available position, available piece)
+        """
+        # retrieve the available positions.
+        # These positions are returned as [x, y] coordinates.
+        available_pos = self.free_spots()
+        # # flat down the grid.
+        # flat_available_pos = [pos[0] * 4 + pos[1] for pos in available_pos]
+
+        # return the array of available actions.
+        # return np.fromiter(product(flat_available_pos, self.available_pieces), dtype = tuple)
+        return np.fromiter(product(available_pos, self.available_pieces), dtype = tuple)
+
+    
+
+
+
+        
