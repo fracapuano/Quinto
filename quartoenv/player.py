04fe5e949aeb7f1baa08250388df2ee61eb3dbfa
fracapuano <francesco_capuano@aol.com>
2022-12-24 19:59:12 +0100

    player class implementing both random player and API to accept human player

--- quartoenv/player.py
+++ quartoenv/player.py
@@ -1,13 +1,21 @@
 import logging
 import random
-from .game import QuartoPiece, QuartoGame
 from .env import QuartoEnv
 
 logger = logging.getLogger(__name__)
 
-class RandomPlayer:
-    def __init__(self, env):
-        assert isinstance(env.unwrapped, QuartoEnv), env
+class Player: 
+    def reset(self): 
+        ...
+
+    def seed(self, seed:int):
+        random.seed(seed)
+
+class RandomPlayer(Player):
+    def __init__(self, env:QuartoEnv):
+        if not isinstance(env, QuartoEnv):
+            raise ValueError(f"Env {env} is not a valid env! Submit a `Quarto Env`!")
+        
         self.env = env
 
     def reset(self):
@@ -17,10 +25,14 @@ class RandomPlayer:
         possible_actions = list(self.env.legal_actions)
         return random.choice(possible_actions), None
 
-    def seed(self, seed):
-        random.seed(seed)
+class HumanPlayer(Player):
 
-class HumanPlayer:
+    def __init__(self, env:QuartoEnv): 
+        if not isinstance(env, QuartoEnv):
+            raise ValueError(f"Env {env} is not a valid env! Submit a `Quarto Env`!")
+        
+        self.env = env
+
     def reset(self):
         pass
 
@@ -31,11 +43,15 @@ class HumanPlayer:
         # print available pieces. Print the integer and not the QuartoInstance, which is more informative.
         available_pieces = [piece.index for piece in self.env.game.available_pieces]
         print(available_pieces)
-        # TODO: WE NEED TO FIND A WAY TO INCLUDE THE OLD PIECE, SO THE PLAYER KNOWS WHAT THEY HAVE TO PLACE.
-        input_x, input_y = input("Where would you like to place the piece? Please indicate X and Y, divided by a blank space -> e.g. 0 3")
+
+        print(f"Where would you like to place piece {self.env.piece}?")
+        input_str = input("Please indicate X and Y, divided by a blank space -> e.g. 0 3")
+        input_x, input_y = input_str.split(" ")  # split input command on white space
+
         position = int(input_x) * 4 + int(input_y)
         input_piece = input("What piece do you want me to place next?")
-        next_piece = input_piece.index
+        
+        next_piece = int(input_piece)
     
         return (position, next_piece), None
 
