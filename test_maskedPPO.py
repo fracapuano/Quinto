fix: changed mlppolicy to MaskableActorCriticPolicy. Also removed duplicates from masking

test_maskedPPO.py
@@ -3,27 +3,14 @@ import numpy as np
 from quartoenv import RandomOpponentEnv
 from sb3_contrib.common.wrappers import ActionMasker
 from sb3_contrib.ppo_mask import MaskablePPO
-from itertools import product
-
-# def mask_function(env: gym.Env) -> np.ndarray:
-#     """This function returns the encoding of the valid moves given the actual
-#     """
-#     # work out all actions: [(0, 0), ..., (15, 15)]
-#     all_actions = product(range(16), range(16))
-#     # find all legal actions
-#     legal_actions = list(env.legal_actions())
-  
-#     return [action in legal_actions for action in all_actions]
-#     # # return masking
-#     # for action in all_actions:
-#     #     yield action in legal_actions
+from sb3_contrib.common.maskable.policies import MaskableActorCriticPolicy
 
 def mask_function(env: gym.Env) -> np.ndarray:
     """This function returns the encoding of the valid moves given the actual.
     """
     # unpack legal_actions() in legal_positions and in legal_pieces
-    legal_positions = [action[0] for action in env.legal_actions()]
-    legal_pieces = [action[1] for action in env.legal_actions()]
+    legal_positions = set([action[0] for action in env.legal_actions()])
+    legal_pieces = set([action[1] for action in env.legal_actions()])
 
     # convert into masking
 
@@ -39,5 +26,7 @@ def mask_function(env: gym.Env) -> np.ndarray:
 env = RandomOpponentEnv()
 env = ActionMasker(env = env, action_mask_fn = mask_function)
 
-model = MaskablePPO(policy="MlpPolicy", env=env, verbose=1)
-model.learn(total_timesteps=10_000, progress_bar=True)
+model = MaskablePPO(MaskableActorCriticPolicy, env=env, verbose=2)
+timesteps = 1_000_000
+model.learn(total_timesteps=timesteps, progress_bar=True)
+model.save(f'trainedmodels/maskedPPO_1e6.mdl')