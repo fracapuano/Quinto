23ca81915c345f0a51da568a2606cf4403139e55
fracapuano <francesco_capuano@aol.com>
2022-12-28 16:12:02 +0100

    refactoring

--- test_v0/test_PPO.py
+++
@@ -1,8 +0,0 @@
-from stable_baselines3 import PPO
-from quartoenv import RandomOpponentEnv
-
-env = RandomOpponentEnv()
-model = PPO("MlpPolicy", env=env, verbose=1)
-
-model.learn(total_timesteps=1e6, progress_bar=True)
-model.save("PPOv1_1e6.mdl")
