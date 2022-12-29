2615f01cc9f7b95308769a030e9e091ba6d80b62
fracapuano <francesco_capuano@aol.com>
2022-12-29 23:26:48 +0100

    remove: refactoring

--- policies/__init__.py
+++
@@ -1,4 +0,0 @@
-from .randompolicy import * 
-from .masked_randompolicy import *
-from .A2CPolicy import QuartoA2C
-from .PPOPolicy import QuartoPPO
--- quartoenv/__init__.py
+++
@@ -1,4 +0,0 @@
-from .env import RandomOpponentEnv
-from .base_env import QuartoBase
-from .encoder import MoveEncoder
-from .game import *
