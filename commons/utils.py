ed42da6b124586e23b7ce0c072a881c0d8a8b14a
fracapuano <francesco_capuano@aol.com>
2023-01-23 01:37:06 +0100

    add: update opponent while training and symmetry exploitation

--- commons/utils.py
+++ commons/utils.py
@@ -1,6 +1,8 @@
 from .policies import *
 from typing import Union
 from stable_baselines3.common.callbacks import BaseCallback
+import os
+from typing import Tuple
 
 class WinPercentageCallback(BaseCallback):
     """
@@ -64,6 +66,32 @@ class WinPercentageCallback(BaseCallback):
             )
         return True
 
+class UpdateOpponentCallback(BaseCallback):
+    """
+    A custom callback that derives from ``BaseCallback``.
+
+    :param verbose: (int) Verbosity level 0: not output 1: info 2: debug
+    """
+    def __init__(self, checkpoints_dir:str="checkpoints/", verbose:int=0):
+        super().__init__(verbose)
+        self.checkpoint_dir = checkpoints_dir
+
+    def _on_step(self) -> bool:
+        """
+        This method will be called by the model after each call to `_env.step()`.
+
+        For child callback (of an `EventCallback`), this will be called
+        when the event is triggered.
+
+        :return: (bool) If the callback returns False, training is aborted early.
+        """
+        # access last element in listdir - usually mostly trained agent ~ best available
+        candidate = MaskablePPO.load(os.listdir(self.checkpoint_dir)[-1])
+        # updating the current opponent with new candidate
+        self.model.env.update_opponent(candidate)
+
+        return True
+
 def preprocess_policy(_env:gym.Env, policy:Union[OnPolicyAlgorithm, str])->Union[MaskedRandomPolicy, RandomPolicy, OnPolicy]:
         """This function pre-processes the input policy returning an instance of the policy that
         exposes a `train_policy` method.
@@ -112,3 +140,60 @@ class QuartoPolicy:
         """Tests considered policy over `n_episodes`"""
         return self._policy.test_policy(n_episodes=n_episodes, verbose=verbose)
 
+def apply_symmetries(board:np.array)->Tuple[np.array, list, list]:
+    """This function applies the Quarto game symmetries to a given board, returning the board considered
+    and the function that can be used to re-obtain original form (i.e., the function that when applied on apply_symmetries output
+    would output board input once more)
+
+    Args: 
+        board (np.array): original board to translate to its canonical form
+    
+    Returns: 
+        Tuple[np.array, list, list]: board in the canonical form, list of functions used for canonization and list of functions that 
+        can be used to revert canonization
+    """
+    # Define a list of all possible symmetries in a game of Quarto
+    symmetries = {
+        # Rotate 90 degrees
+        "rot90": lambda x: np.rot90(x, k=1),
+        # Rotate 180 degrees
+        "rot180": lambda x: np.rot90(x, k=2),
+        # Rotate 270 degrees
+        "rot270": lambda x: np.rot90(x, k=3),
+        # Reflect horizontally
+        "h_flip": lambda x: np.fliplr(x),
+        # Reflect vertically
+        "v_flip": lambda x: np.flipud(x)
+    }
+    # Define inverse symmetries
+    inverse_symmetries = {
+        # Rotate -90 degrees (270 degrees)
+        "rot90": lambda x: np.rot90(x, k=3),
+        # Rotate -180 degrees (180 degrees)
+        "rot180": lambda x: np.rot90(x, k=2),
+        # Rotate -270 degrees (90 degrees)
+        "rot270": lambda x: np.rot90(x, k=3),
+        # Reflect horizontally, once more
+        "h_flip": lambda x: np.fliplr(x),
+        # Reflect vertically, once more
+        "v_flip": lambda x: np.flipud(x)
+    }
+
+    inv_symmetries = []
+    applied_symmetries = []
+    
+    # applies symmetries
+    for name, symmetry in symmetries.items():
+        # applies the symmetry
+        sym_board = symmetry(board)
+        # stores the reflected (updated board state)
+        applied_symmetries.append(sym_board)
+        # stores the new symmetrical representation
+        inv_symmetries.append(inv_symmetries[name])
+
+    canonical_board = min(applied_symmetries)
+    applied_idxs = applied_symmetries.index(canonical_board)
+
+    return canonical_board, list(symmetries.values()).index(canonical_board), reversed(inv_symmetries.index())
+
+"""TODO: Implement (x,y)_canonical to (x,y)_original having the list of applied symmetries"""
