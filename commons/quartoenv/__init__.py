1149c68df0103e444ad0ecffe31f7887a2b9160b
fracapuano <francesco_capuano@aol.com>
2023-01-23 01:39:05 +0100

    add: custom opponent

--- commons/quartoenv/__init__.py
+++ commons/quartoenv/__init__.py
@@ -1,6 +1,7 @@
 from .env import RandomOpponentEnv
 from .env_v1 import RandomOpponentEnv_V1
 from .env_v2 import RandomOpponentEnv_V2
+from .env_v3 import RandomOpponentEnv_V3
 from .base_env import QuartoBase
 from .encoder import MoveEncoder
 from .game import *
